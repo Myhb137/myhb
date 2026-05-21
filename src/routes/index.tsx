6d69d967d989ce230ca4c743ea33c3c0225e3b69
Myhb137 <noreply@github.com>
2026-05-21 20:07:13 +0100

    myhb14

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -42,11 +42,11 @@ export const Route = createFileRoute("/")({
       {
         rel: "icon",
         type: "image/png",
-        href: "./artificial-intelligence.png",
+        href: "./public/artificial-intelligence.png",
       },
       {
         rel: "apple-touch-icon",
-        href: "./artificial-intelligence.png",
+        href: "./public/artificial-intelligence.png",
       },
     ],
   }),
